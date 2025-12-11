db052d16d3beeab94d83985a885643e5d7031650
Joowyy <joelsanchezfernandez05@gmail.com>
2025-12-12 00:07:21 +0100

    Organizacion de Carpetas, AppCompat, MainLayout, Implentation en el build.gradle + viewBinding, y navGraph

--- app/src/main/java/com/example/joelsanchez_pokeapi/MainActivity.kt
+++ app/src/main/java/com/example/joelsanchez_pokeapi/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -13,7 +14,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.joelsanchez_pokeapi.ui.theme.JoelSanchez_PokeAPITheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : MainActivity
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
