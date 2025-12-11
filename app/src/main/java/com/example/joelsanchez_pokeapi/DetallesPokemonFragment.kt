8130c9e156bb0a8f9be719ac4666933aca442b3f
Joowyy <joelsanchezfernandez05@gmail.com>
2025-12-12 00:38:21 +0100

    Fondos de pantalla

--- app/src/main/java/com/example/joelsanchez_pokeapi/DetallesPokemonFragment.kt
+++ app/src/main/java/com/example/joelsanchez_pokeapi/DetallesPokemonFragment.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.joelsanchez_pokeapi.databinding.FragmentDetallesPokemonBinding
-import com.example.joelsanchez_pokeapi.databinding.FragmentPokemonBinding
 
 class DetallesPokemonFragment : Fragment() {
 
