76b0ff034533862cbec1acaec718233149c09fb6
Joowyy <joelsanchezfernandez05@gmail.com>
2025-12-25 12:50:11 +0100

    Clase Pokemon y PokemonRepository.kt, Fotos pikachu y estrellasFav

--- app/src/main/java/com/example/joelsanchez_pokeapi/model/Pokemon.kt
+++ app/src/main/java/com/example/joelsanchez_pokeapi/model/Pokemon.kt
@@ -1,16 +1,28 @@
 package com.example.joelsanchez_pokeapi.model
 
 import java.io.Serializable
+import java.util.Objects
 
 class Pokemon (
 
-    val nombre: String,
+    val nombre: String?,
     val imagen: Int,
-    val descripcion: String,
-    val tipo: String
+    val descripcion: String?,
+    val tipo: String?,
+    var favorito: Boolean = false
 
 ) : Serializable {
 
+    override fun hashCode(): Int {
 
+        return Objects.hash(nombre, imagen, descripcion, tipo, favorito)
+
+    }
+
+    fun camAtributoFavorito (favorito : Boolean) {
+
+        this.favorito = favorito
+
+    }
 
 }
--- app/src/main/java/com/example/joelsanchez_pokeapi/repository/PokemonRepository.kt
+++ app/src/main/java/com/example/joelsanchez_pokeapi/repository/PokemonRepository.kt
@@ -1,7 +1,27 @@
 package com.example.joelsanchez_pokeapi.repository
 
+import com.example.joelsanchez_pokeapi.R
+import com.example.joelsanchez_pokeapi.model.Pokemon
+
 class PokemonRepository {
 
+    private val listaPokemon : MutableList<Pokemon> = mutableListOf(
 
+        Pokemon("Pikachu", R.drawable.pikachu, "Rata electrica", "Eléctrico", false)
+
+    )
+
+    fun actualizarPokemon (pokemon : Pokemon?) {
+
+        // Recuperamos la posicion en la que se encuentra el pokemon cambiado.
+        val posicion = listaPokemon.indexOf(pokemon)
+
+        // Y le aniadimos las modificaciones que se llevaron a cabo.
+        listaPokemon[posicion] = pokemon!!
+
+    }
+
+    // Getter de Pokemons
+    fun getPokemons() : MutableList<Pokemon> = listaPokemon
 
 }
